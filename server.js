b47f49c033f58ee47f56e37a1d3573884cb9e9e0
ajay <ajayraj3461@gmail.com>
2025-04-09 13:13:16 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 
+const express = require('express');
+const cors = require('cors');
+const jobRoutes = require('./routes/jobRoutes'); // adjust the path if needed
 
-// 7. backend/server.js
-const app = require('./app');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/', jobRoutes);
+
+// Server listening
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
